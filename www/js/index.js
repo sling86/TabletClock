75b15063b2dd0044af63d21a54e001540de3879a
Tony Malins <tony.malins@contractcandles.com>
2014-03-28 17:23:10 +0000

    Fix JS

--- www/js/index.js
+++ www/js/index.js
@@ -37,19 +37,8 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        //var parentElement = document.getElementById(id);
-        //var listeningElement = parentElement.querySelector('.listening');
-        //var receivedElement = parentElement.querySelector('.received');
-
-        //listeningElement.setAttribute('style', 'display:none;');
-        //receivedElement.setAttribute('style', 'display:block;');
-
-        //console.log('Received Event: ' + id);
-		
-		
-		
-		
-var currentBlink = "hidden";
+		updateClock(); setInterval('updateClock()', 1000 )
+		var currentBlink = "hidden";
 
 function updateClock ( )
 {
@@ -86,13 +75,15 @@ function updateClock ( )
 	document.getElementById("clock").innerHTML = currentTimeString;
 
 }
+		
+        //var parentElement = document.getElementById(id);
+        //var listeningElement = parentElement.querySelector('.listening');
+        //var receivedElement = parentElement.querySelector('.received');
 
-		
-		
-		
-		
-		
-		
+        //listeningElement.setAttribute('style', 'display:none;');
+        //receivedElement.setAttribute('style', 'display:block;');
+
+        //console.log('Received Event: ' + id);		
 		
     }
 };
