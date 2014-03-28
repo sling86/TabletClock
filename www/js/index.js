bb37c9ae0950d2d6b03c63e8370ef8112bc06cf4
Tony Malins <tony.malins@contractcandles.com>
2014-03-28 17:01:32 +0000

    Clock Base

---
+++ www/js/index.js
@@ -0,0 +1,98 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicity call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        //var parentElement = document.getElementById(id);
+        //var listeningElement = parentElement.querySelector('.listening');
+        //var receivedElement = parentElement.querySelector('.received');
+
+        //listeningElement.setAttribute('style', 'display:none;');
+        //receivedElement.setAttribute('style', 'display:block;');
+
+        //console.log('Received Event: ' + id);
+		
+		
+		
+		
+var currentBlink = "hidden";
+
+function updateClock ( )
+{
+	if(currentBlink == "hidden"){
+		currentBlink = "visible";
+	}
+	else{
+		currentBlink = "hidden";
+	}
+	var currentTime = new Date ( );
+	var currentHours = currentTime.getHours ( );
+	var currentMinutes = currentTime.getMinutes ( );
+	var currentSeconds = currentTime.getSeconds ( );
+	
+	// Pad the minutes and seconds with leading zeros, if required
+	currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
+	currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
+	
+	// Choose either "AM" or "PM" as appropriate
+	var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";
+	
+	// Convert the hours component to 12-hour format if needed
+	currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
+	
+	// Convert an hours component of "0" to "12"
+	currentHours = ( currentHours == 0 ) ? 12 : currentHours;
+	
+	// Compose the string for display
+	//var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
+	var currentTimeString = currentHours + "<span id='blinkin' style='visibility:" + currentBlink + "'>:</span>" + currentMinutes + " " + timeOfDay;
+	
+	// Update the time display
+	//document.getElementById("clock").firstChild.nodeValue = currentTimeString;
+	document.getElementById("clock").innerHTML = currentTimeString;
+
+}
+
+		
+		
+		
+		
+		
+		
+		
+    }
+};
