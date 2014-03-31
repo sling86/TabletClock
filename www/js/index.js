3e02dc6bf8cb67e3de8366f2b485fe1d059183d8
Tony Malins <tony.malins@contractcandles.com>
2014-03-31 15:13:30 +0100

    Hopefull

--- www/js/index.js
+++ www/js/index.js
@@ -43,31 +43,31 @@ var app = {
 		var currentBlink = "hidden";		
         
 		function updateClock ( ) {
-		if(currentBlink == "hidden"){
-			currentBlink = "visible";
-		} else {
-			currentBlink = "hidden";
+			if(currentBlink == "hidden"){
+				currentBlink = "visible";
+			} else {
+				currentBlink = "hidden";
+			}
+			var currentTime = new Date ( );
+			var currentHours = currentTime.getHours ( );
+			var currentMinutes = currentTime.getMinutes ( );
+			var currentSeconds = currentTime.getSeconds ( );
+			// Pad the minutes and seconds with leading zeros, if required
+			currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
+			currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
+			// Choose either "AM" or "PM" as appropriate
+			var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";
+			// Convert the hours component to 12-hour format if needed
+			currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
+			// Convert an hours component of "0" to "12"
+			currentHours = ( currentHours == 0 ) ? 12 : currentHours;
+			// Compose the string for display
+			//var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
+			var currentTimeString = currentHours + "<span id='blinkin' style='visibility:" + currentBlink + "'>:</span>" + currentMinutes + " " + timeOfDay;
+			// Update the time display
+			//document.getElementById("clock").firstChild.nodeValue = currentTimeString;
+			document.getElementById("clock").innerHTML = currentTimeString;
 		}
-		var currentTime = new Date ( );
-		var currentHours = currentTime.getHours ( );
-		var currentMinutes = currentTime.getMinutes ( );
-		var currentSeconds = currentTime.getSeconds ( );
-		// Pad the minutes and seconds with leading zeros, if required
-		currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
-		currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
-		// Choose either "AM" or "PM" as appropriate
-		var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";
-		// Convert the hours component to 12-hour format if needed
-		currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
-		// Convert an hours component of "0" to "12"
-		currentHours = ( currentHours == 0 ) ? 12 : currentHours;
-		// Compose the string for display
-		//var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
-		var currentTimeString = currentHours + "<span id='blinkin' style='visibility:" + currentBlink + "'>:</span>" + currentMinutes + " " + timeOfDay;
-		// Update the time display
-		//document.getElementById("clock").firstChild.nodeValue = currentTimeString;
-		document.getElementById("clock").innerHTML = currentTimeString;
-	}
-		//$('.app').textfill({ maxFontPixels: 36 });
+		$('.app').textfill({ maxFontPixels: 36 });
     }
 };
